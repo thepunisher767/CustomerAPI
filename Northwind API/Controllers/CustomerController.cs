e107bcac32f7d097eb58a2e667b8dd574092161d
zscholts <zachscholts@quickenloans.com>
2020-09-24 14:01:41 -0400

    initial

---
+++ Northwind API/Controllers/CustomerController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Data;
+using Dapper.Contrib;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using System.Data.SqlClient;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+namespace Northwind_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ILogger<CustomerController> _logger;
+        private IDbConnection _db;
+
+        public CustomerController(ILogger<CustomerController> logger, IDbConnection db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        [HttpPost("Add")]
+        public void Add(Customers cust)
+        {
+            Customers.AddCustomer(_db, cust);
+        }
+
+        [HttpDelete("Delete")]
+        public void Delete(Customers cust)
+        {
+            string customerid = cust.CustomerID;
+            Customers.DeleteCustomer(_db, customerid);
+        }
+
+        [HttpGet("CustInfo/{startswith}")]
+        public List<Customers> Get(string startswith)
+        {
+            List<Customers> customerlist = Customers.GetCustomers(_db, startswith);
+            return customerlist;
+        }
+
+        [HttpGet("CustInfo/Region/{Region}")]
+        public List<Customers> GetRegion(string region)
+        {
+            List<Customers> regionlist = Customers.GetRegionCustomers(_db, region);
+            return regionlist;
+        }
+    }
+}
---
+++ Northwind API/Customers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using Newtonsoft.Json.Linq;
+
+namespace Northwind_API
+{
+    [Table("Customers")]
+    public class Customers
+    {
+        [ExplicitKey]
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+
+        public static List<Customers> GetCustomers(IDbConnection db, string startswith)
+        {
+            List<Customers> cust = db.Query<Customers>($"SELECT * FROM Customers WHERE CustomerID LIKE '{startswith}%' ORDER BY CustomerID").ToList();
+            return cust;
+        }
+
+        public static List<Customers> GetRegionCustomers(IDbConnection db, string region)
+        {
+            List<Customers> regionlist = db.Query<Customers>($"SELECT * FROM Customers WHERE Region LIKE '%{region}%' ORDER BY Region").ToList();
+            return regionlist;
+        }
+
+        public static void AddCustomer(IDbConnection db, Customers cust)
+        {
+            db.Insert<Customers>(cust);
+        }
+
+        public static void DeleteCustomer(IDbConnection db, string customerid)
+        {
+            Customers delete = db.Get<Customers>(customerid);
+            if (delete != null)
+            {
+                db.Delete<Customers>(new Customers() { CustomerID = customerid });
+            }
+        }
+    }
+}
